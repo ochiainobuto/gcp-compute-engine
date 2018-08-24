0590370d53caee6e0ba1a751e784235e672fe1a2
ochiainobuto <nob55@nifty.com>
2018-08-24 17:10:54 +0900

    Update app.py

--- app_v1/app.py
+++ app_v1/app.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, request, redirect, url_for
-from flask import escape, Markup
-from wtforms import Form, TextAreaField, StringField, validators, ValidationError
-import arrow
+from flask import Flask
+
 
 app = Flask(__name__)
 
@@ -18,3 +16,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, debug=False)
+
