b8d6b2b2106c794e7ed905f976070300facc88b8
ochiainobuto <nob55@nifty.com>
2018-08-24 17:02:35 +0900

    Update app.py

--- app_v1/app.py
+++ app_v1/app.py
@@ -1,48 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-from flask import Flask, request, redirect, url_for, send_from_directory
-from werkzeug import secure_filename
-#from google.cloud import storage
+from flask import Flask, render_template, request, redirect, url_for
+from flask import escape, Markup
+from wtforms import Form, TextAreaField, StringField, validators, ValidationError
+import arrow
 
-UPLOAD_FOLDER = 'test'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
 
 app = Flask(__name__)
 
 
-def allowed_file(filename):
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        file = request.files['file']
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            path_list = [UPLOAD_FOLDER, filename]
-            print('path_list',path_list)
-            file.save(os.path.join(*path_list))
-
-            return redirect(url_for('uploaded_file', filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
-
-
-@app.route('/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(UPLOAD_FOLDER, filename)
 
 
 if __name__ == '__main__':
