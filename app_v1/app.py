0ba6c7d7d413bf549c1ee9b91005528b4efd2d5c
ochiainobuto <nob55@nifty.com>
2018-08-24 16:46:44 +0900

    Update app.py

--- app_v1/app.py
+++ app_v1/app.py
@@ -1,66 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, request, redirect, url_for
-from flask import escape, Markup
-from wtforms import Form, TextAreaField, StringField, validators, ValidationError
-import arrow
+import os
+from flask import Flask, request, redirect, url_for, send_from_directory
+from werkzeug import secure_filename
+#from google.cloud import storage
 
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import desc
+UPLOAD_FOLDER = 'test'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///message.db'
-db = SQLAlchemy(app)
 
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    timestamp = db.Column(db.String(19))
-    name = db.Column(db.String(16))
-    message = db.Column(db.String(1024))
-    filename = db.Column(db.String(128))
 
-    def __init__(self, timestamp, name, message, filename):
-        self.timestamp = timestamp
-        self.name = name
-        self.message = message
-        self.filename = filename
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-db.create_all()
+@app.route('/', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
 
-@app.template_filter('add_br')
-def linesep_to_br_filter(s):
-    return escape(s).replace('\n', Markup('<br>'))
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            path_list = [UPLOAD_FOLDER, filename]
+            print('path_list',path_list)
+            file.save(os.path.join(*path_list))
 
-class MessageForm(Form):
-    input_name = StringField(u'お名前', [validators.Length(min=1, max=16)])
-    input_message = TextAreaField(u'メッセージ',
-                                  [validators.Length(min=1, max=1024)])
+            return redirect(url_for('uploaded_file', filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/messages')
-def messages():
-    form = MessageForm(request.form)
-    last_messages = Message.query.order_by(desc(Message.id)).limit(5)
-    last_messages = [message for message in last_messages]
-    last_messages.reverse()
-    return render_template('messages.html', form=form, messages=last_messages)
+@app.route('/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
 
-@app.route('/post', methods=['POST'])
-def post():
-    form = MessageForm(request.form)
-    if request.method == 'POST' and form.validate():
-        timestamp = arrow.utcnow().to('Asia/Tokyo').format('YYYY/MM/DD HH:mm:ss')
-        name = request.form['input_name']
-        message = request.form['input_message']
-        db.session.add(Message(timestamp, name, message, filename=None))
-        db.session.commit()
-        return render_template('post.html', name=name, timestamp=timestamp)
-    else:
-        return redirect(url_for('messages'))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, debug=False)
